ad933d456d3aa60fd7e30a2075e4d44dccae9f26
LuisHMP0 <148148975+LuisHMP0@users.noreply.github.com>
2024-11-14 11:45:44 -0300

    (feat, style): just adding skeleton loading

--- front/src/components/Shop/Product/Product.tsx
+++ front/src/components/Shop/Product/Product.tsx
@@ -1,23 +1,26 @@
 import './Product.css'
 import bookImg from './imgs/book.jpg'
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css'
 
 type ProductProps = {
     img: string;
     title: string;
     author: string;
     price: number; 
+    isLoading: boolean;
 };
 
-const Product: React.FC<ProductProps> = ({img, title, author, price}) => {
+const Product: React.FC<ProductProps> = ({img, title, author, price, isLoading}) => {
   return (
         <div className='productTemplate'>
             <div className='productImg'>
-                <img src={img || bookImg} alt="book-img" />
+                {isLoading ? ( <Skeleton height={310} width={285} /> ) : ( <img src={img || bookImg} alt="book-img" /> )}
             </div>
             <div className='productProperties'>
-                <h1> {title || 'title-undefined'} </h1>
-                <h2> {author || 'author-undefined'} </h2>
-                <p> {'R$' + price || 'price-undefined'} </p>
+                <h1>{isLoading ? <Skeleton height={30} width={260} /> : title}</h1>
+                <h2>{isLoading ? <Skeleton height={15} width={130} /> : author}</h2>
+                <p>{isLoading ? <Skeleton height={10} width={60} /> : 'R$' + price}</p>
             </div>
             <div className='details'>
                 <button className='seeDetails'> See Details </button>
--- front/src/components/Shop/Products/Products.tsx
+++ front/src/components/Shop/Products/Products.tsx
@@ -2,6 +2,7 @@ import Product from '../Product/Product'
 import './Products.css'
 import {useEffect, useState} from 'react'
 
+
 type Book = {
   title: string;
   author: string; 
@@ -12,6 +13,7 @@ type Book = {
 const Products = () => {
 
   const [books, setBooks] = useState<Book[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -29,6 +31,8 @@ const Products = () => {
         setBooks(bookData);
       } catch (error) {
         console.error('Erro ao buscar livros:', error);
+      } finally {
+        setIsLoading(false);
       }
     };
 
@@ -39,15 +43,27 @@ const Products = () => {
     <>
     <section className='filters'></section>
     <section className='products'>
-      {books.map((book, index) => (
-        <Product
-          key={index}
-          img={book.coverImage}
-          title={book.title}
-          author={book.author}
-          price={book.price}
-        />
-      ))}
+      {isLoading
+        ? Array.from({ length: 8 }).map((_, index) => ( // Exibe 8 skeletons enquanto carrega
+            <Product
+              key={index}
+              img=""
+              title=""
+              author=""
+              price={0}
+              isLoading={true}
+            />
+          ))
+        : books.map((book, index) => (
+            <Product
+              key={index}
+              img={book.coverImage}
+              title={book.title}
+              author={book.author}
+              price={book.price}
+              isLoading={false}
+            />
+          ))}
     </section>
     </>
   )
